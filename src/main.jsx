7c5cd9646adf4f75770ad82a8cf0e6880b7a06d8
emreturkoglu1 <emreturkoglu@posta.mu.edu.tr>
2026-04-29 20:30:30 +0300

    fast

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { App as AdminApp } from '../../frontend/src/App'
+import { AuthProvider } from '../../frontend/src/lib/auth'
+import '../../frontend/src/index.css'
+
+const queryClient = new QueryClient()
+const isAdminPath = /^\/(admin|login)(\/|$)/.test(window.location.pathname)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {isAdminPath ? (
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <BrowserRouter>
+            <AdminApp />
+          </BrowserRouter>
+        </AuthProvider>
+      </QueryClientProvider>
+    ) : (
+      <App />
+    )}
   </StrictMode>,
 )
